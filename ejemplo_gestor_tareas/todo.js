89d3380e0857edaeaa526768332f0e7da8639cc2
Dementes <julian1dave1@hotmail.com>
2025-11-21 17:00:45 -0500

    generar estilosal HTML

--- ejemplo_gestor_tareas/todo.js
+++ ejemplo_gestor_tareas/todo.js
@@ -1,23 +1,29 @@
 const pantalla=document.getElementById('pantalla');
 const button=document.getElementById('agregar');
-const ul=document.getElementById('content-list');
+const ul=document.getElementById('lista_tareas');
 
 button.onclick=()=>{
     let li = document.createElement('li');
+    li.className = "tarea";
 
     let texto = document.createElement('span');
     texto.textContent = pantalla.value;
 
+    texto.onclick = () => {texto.classList.toggle("tachar");};
+
     let boton_eliminar = document.createElement("button");
     boton_eliminar.textContent = "Eliminar";
+    boton_eliminar.className = "eliminar";
     boton_eliminar.onclick = () => li.remove();
 
     let boton_actualizar = document.createElement("button");
     boton_actualizar.textContent = "Actualizar";
+    boton_actualizar.className = "actualizar";
     boton_actualizar.onclick = () => {
         
         pantalla.value = texto.textContent;
         texto.textContent = "";
+        
         button.onclick = () => {
             texto.textContent = pantalla.value;
             pantalla.value = "";
