781a71aa72f8a6cf40d4a9c5196c7310013912d1
Dementes <julian1dave1@hotmail.com>
2025-11-26 17:08:46 -0500

    implementar clases gato y tortuga

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 from POO.persona import Persona
 from POO.automovil import Automovil
 from POO.perro import Perro
+from POO.tortuga import Tortuga
+from POO.gato import Gato
 
 auto1 = Automovil("rojo")
 auto2 = Automovil("amarillo")
@@ -10,8 +12,13 @@ auto1.arrancar()
 auto2.arrancar()
 auto3.arrancar()
 
-persona1 = Persona("YOOOOOO")
+persona1 = Persona("JULIAN")
 persona1.acciones()
 
 perro1 = Perro("firulas", "pug", 3, "largo")
 perro1.ladrar()
+tortuga1 = Tortuga("dios", "tortuga", 10, "si")
+tortuga1.comer()
+gato1 = Gato("misifus", "gato", 10, "largo")
+gato1.maullar()
+print(perro1.tipoPelo)
