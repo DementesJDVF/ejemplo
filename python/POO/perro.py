f793d0e3acbb68821fe68ff400e76c584fec3153
Dementes <julian1dave1@hotmail.com>
2025-11-26 22:16:39 -0500

    polimorfismo en presentar y hacer sonido enfocado al perro

--- python/POO/perro.py
+++ python/POO/perro.py
@@ -5,5 +5,11 @@ class Perro(Mascota):
         super().__init__(nombre, raza, edad)
         self.tipoPelo = tipoPelo
         
-    def ladrar(self):
-        print("guaaaau guaaaau")
+    def hacerSonido(self):
+        print("guaaaau guaaaau, hace el perro", self.getNombre())
+    
+    def presentar (self):
+        print("Soy un perro de nombre: ", self.getNombre().upper(),
+            "\nSoy de raza:", self.getRaza().upper(), 
+            "\nTengo:", self.getEdad(), 
+            "años de edad\nMi pelo es:", self.tipoPelo)
