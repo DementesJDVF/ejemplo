3b1648fe17647995ade5baa13474a34957fe160a
Dementes <julian1dave1@hotmail.com>
2025-11-26 22:17:17 -0500

    crear getter y setter para herencia

--- python/POO/mascota.py
+++ python/POO/mascota.py
@@ -1,5 +1,23 @@
 class Mascota:
     def __init__(self, nombre, raza, edad):
-        self.nombre = nombre
-        self.raza = raza
-        self.edad = edad
+        self.__nombre = nombre
+        self.__raza = raza
+        self.__edad = edad
+
+    def getNombre(self):
+        return self.__nombre
+
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+
+    def getRaza(self):
+        return self.__raza
+
+    def setRaza(self, raza):
+        self.__raza = raza
+
+    def getEdad(self):
+        return self.__edad
+
+    def setEdad(self, edad):
+        self.__edad = edad
